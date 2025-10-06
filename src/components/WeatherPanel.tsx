505f01afe2a6751ea00fb826b6d619145c9bcea7
Hans <56239179+hansthedude@users.noreply.github.com>
2025-10-06 01:08:36 +0100

    - Include icons-react - Add icons for all weather metrics

--- src/components/WeatherPanel.tsx
+++ src/components/WeatherPanel.tsx
@@ -1,4 +1,5 @@
 import { Group, Image, Stack, Text } from "@mantine/core";
+import { IconArrowUp, IconDroplet, IconShirt, IconTemperature, IconTemperatureMinus, IconTemperaturePlus, IconWind } from "@tabler/icons-react";
 
 export function WeatherPanel({metricWeatherData, imperialWeatherData}: {metricWeatherData: any, imperialWeatherData: any}) {
     return (
@@ -10,14 +11,35 @@ export function WeatherPanel({metricWeatherData, imperialWeatherData}: {metricWe
                     <Text size="xs">{metricWeatherData.weather[0].description}</Text>
                 </Group>
                 <br/>
-                <Text>Current Temperature: {metricWeatherData.main.temp}°C</Text>
-                <Text>Feels Like: {metricWeatherData.main.feels_like}°C</Text>
-                <Text>Humidity: {metricWeatherData.main.humidity}%</Text>
-                <Text>Min Temperature: {metricWeatherData.main.temp_min}°C</Text>
-                <Text>Max Temperature: {metricWeatherData.main.temp_max}°C</Text>
+                <Group>
+                    <IconTemperature size={16} />
+                    <Text>Current Temperature: {metricWeatherData.main.temp}°C</Text>
+                </Group>
+                <Group>
+                    <IconShirt size={16} />
+                    <Text>Feels Like: {metricWeatherData.main.feels_like}°C</Text>
+                </Group>
+                <Group>
+                    <IconDroplet size={16} />
+                    <Text>Humidity: {metricWeatherData.main.humidity}%</Text>
+                </Group>
+                <Group>
+                    <IconTemperatureMinus size={16} />
+                    <Text>Min Temperature: {metricWeatherData.main.temp_min}°C</Text>
+                </Group>
+                <Group>
+                    <IconTemperaturePlus size={16} />
+                    <Text>Max Temperature: {metricWeatherData.main.temp_max}°C</Text>
+                </Group>
                 <br/>
-                <Text>Wind Speed: {imperialWeatherData.wind.speed} mph</Text>
-                <Text>Wind Direction: {metricWeatherData.wind.deg}°</Text>
+                <Group>
+                    <IconWind size={16} />
+                    <Text>Wind Speed: {imperialWeatherData.wind.speed} mph</Text>
+                </Group>
+                <Group>
+                    <IconArrowUp size={16} style={{ transform: `rotate(${metricWeatherData.wind.deg}deg)` }} />
+                    <Text>Wind Direction: {metricWeatherData.wind.deg}°</Text>
+                </Group>
                 <br/>
                 {metricWeatherData.rain ? (<Text>Rain Volume (for the last hour): {metricWeatherData.rain["1h"]} mm</Text>) : null}
             </Stack>
